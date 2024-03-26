90edaaa0e18d2f9097eef216ccf5893832393717
Nurudeen <nurudeenarnold@gmail.com>
2024-03-26 11:06:50 +0200

    Mobile responsiveness

--- script.js
+++ script.js
@@ -100,24 +100,25 @@ function DisplayError(error) {
 }
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelector('section');
-let navLinks = document.querySelector('header nav a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    let top = window.scrollY;
     sections.forEach(sec => {
-        let top = window.scrolly;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
         if(top >= offset && top < offset + height){
-            navLinks.forEach (links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ' ]').classList.add('active');
-            })
+            navLinks.forEach (link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
         }
-    })
-}
+    });
+};
+
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
